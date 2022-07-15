6a3d06102eec0e79b233c0c1602d34108eebbe34
Jack Ross <jjjackross@gmail.com>
2022-07-15 01:30:14 -0500

    Uncommented a line of code that was interfering with production build.

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className="max-w-5xl mx-auto md:grid md:grid-cols-2 flex justify-between">
                 <div className="flex flex-col justify-end">
                     <h3 className="text-slate-700 text-xl font-medium">Links</h3>
-                    <p>If you're reading this give me feedback on the site. This is just a temporary footer I will finish this towards the end of site development.</p>
+                    <p>If you are reading this give me feedback on the site. This is just a temporary footer I will finish this towards the end of site development.</p>
                 </div>
                 <div className="flex justify-end"><LinkedIn /></div>
             </div>
